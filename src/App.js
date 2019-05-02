02f8a88aef394fdce6ce531f5d49d1535db6bb5e
Cody Farrey <codyfarrey@gmail.com>
2019-05-02 12:56:08 -0500

    p6

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import {createStore} from 'redux';
 import './App.css';
 import First from './components/First';
 import Second from './components/Second';
+import reducers from './reducers/index';
 
 function App() {
-    let store = createStore(() => {});
+    let store = createStore(reducers);
     return (
         <Provider store={store}>
             <div className="App">
